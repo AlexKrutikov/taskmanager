2358486ae3a513ca04bbff9e33194b57553715cd
AlexKrutikov <alexkrutik0ff@gmail.com>
2021-04-08 00:01:38 +0300

    wirking version

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const App = {
   data() {
     return {
-      title: 'Список заметок'
+      placeholderString: 'Введите название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+      notes: ['Заметка 1', 'Заметка 2']
+    }
+  },
+  methods: {
+    inputChangeHandler() {
+      this.inputValue = event.target.value
     }
   }
 }
